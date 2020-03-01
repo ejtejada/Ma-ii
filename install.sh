69dfe884ab716450a62f8629b9ecfe6d64b0c987
Jonnyman19 <JonathanContreras92@gmail.com>
2020-03-01 04:56:22 -0800

    updated instalation dependencies/connect to server

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
-sudo apt update; sudo apt install python3 python3-pip cython -y; sudo pip3 install --upgrade pip; sudo pip3 install -U spacy; python3 -m spacy download en_core_web_sm; pip install --user -U nltk; sudo apt install tensorflow-1.13-cuda-10.0 ; sudo pip3 install tensorflow; cd; git clone https://github.com/vsuthichai/paraphraser.git; cd paraphraser; sudo python3 setup.py install; 
-
+sudo apt update; 
+sudo apt install python3 python3-pip cython -y; 
+sudo pip3 install --upgrade pip; 
+sudo pip3 install -U spacy; 
+python3 -m spacy download en_core_web_sm; 
+pip install --user -U nltk; 
+sudo apt install tensorflow-1.13-cuda-10.0 ; 
+sudo pip3 install tensorflow; 
+cd; git clone https://github.com/vsuthichai/paraphraser.git; 
+cd paraphraser; 
+sudo python3 setup.py install; 
+sudo pip3 install -U flask;

1a7ee4b1c29b27539eebff918dcad28fe6404b31
brevsae <edgar_tejada0971@elcamino.edu>
2020-03-01 02:55:18 -0800

    We don't know if this works without a cuda core gpu

--- testSentenceToken.py
+++ testSentenceToken.py
@@ -2,7 +2,7 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 #import /media/friendlysicoach/T1000/Documents/Project_Ma-ii/paraphraser/build/lib/paraphraser/inference
 import paraphraser
 
-	shortenSent = paraphraser()
+shortenSent = paraphraser('train-20180325-001253.tar.gz.tar.gz/model-171856');
 
 data = "All work and no play makes jack dull boy. All work and no play makes jack a dull boy."
 print(sent_tokenize(data))

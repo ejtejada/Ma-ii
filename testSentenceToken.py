f71e661156e089195892e4aadc432cb1ca62d4cd
brevsae <edgar_tejada0971@elcamino.edu>
2020-03-01 02:25:33 -0800

    -.-

--- testSentenceToken.py
+++ testSentenceToken.py
@@ -1,5 +1,8 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 #import /media/friendlysicoach/T1000/Documents/Project_Ma-ii/paraphraser/build/lib/paraphraser/inference
 import paraphraser
+
+	shortenSent = paraphraser()
+
 data = "All work and no play makes jack dull boy. All work and no play makes jack a dull boy."
 print(sent_tokenize(data))

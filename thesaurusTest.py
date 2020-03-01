f9061b01ac51cb2602d7b5451f53079b2aaa90b1
Jacob Gilmartin <32918517+Jgilmartin@users.noreply.github.com>
2020-02-29 17:01:58 -0800

    Update thesaurusTest.py
    Alternate library using PyDictionary

--- thesaurusTest.py
+++ thesaurusTest.py
@@ -1,14 +1,5 @@
-from py_thesaurus import Thesaurus
+from PyDictionary import PyDictionary
 
-tWord = "attack"
+dictionary = PyDictionary("box")
 
-new_instance = Thesaurus(tWord)
-
-# Get the synonyms according to part of speech
-# Default part of speech is noun
-
-print(new_instance.get_synonym())
-print(new_instance.get_synonym(pos='verb'))
-print(new_instance.get_synonym(pos='adj'))
-
-print(new_instance.get_definition())
+print(dictionary.synonym("box"))

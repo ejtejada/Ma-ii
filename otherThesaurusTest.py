3fc2cafbf4ec3bbfb30f2a5f5dbd68c865133ae1
brevsae <edgar_tejada0971@elcamino.edu>
2020-02-29 18:01:33 -0800

    Fixing NLTK usage

--- otherThesaurusTest.py
+++ otherThesaurusTest.py
@@ -1,8 +1,15 @@
 import nltk
 nltk.download('wordnet')
-
+searchWord = "liquid";
+synonyms = []; #Desired set
+antonyms = [];
 from nltk.corpus import wordnet
-
-syns = wordnet.synsets("Liquid")
-print(syns)
-print("Dog")
+for syn in wordnet.synsets(searchWord):
+	for l in syn.lemmas():
+		if searchWord.lower() not in l.name().lower():
+			synonyms.append(l.name());
+			#FIXME remove all words not of the same class as searchWord 
+		if l.antonyms():
+			antonyms.append(l.antonyms()[0].name());
+print(set(synonyms));
+#print(set(antonyms));
